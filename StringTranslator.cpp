1d069c9a9c5abf77704058d78fa8f5ca015b7c2a
krotenko <krotenko@novosoft.net>
2012-12-28 12:45:57 +0700

    new encode/decode algorithm

--- StringTranslator.cpp
+++ StringTranslator.cpp
@@ -1,9 +1,12 @@
 #include "StringTranslator.h"
 
+#include <sstream>
+#include <iomanip>
 
 StringTranslator::StringTranslator(void)
 {
-
+	m_escChar = L'%';
+	m_seqLenght = 2;
 }
 
 
@@ -12,63 +15,34 @@ StringTranslator::~StringTranslator(void)
 
 }
 
-std::wstring StringTranslator::decode(const std::wstring &str)
+void StringTranslator::setEscapeCharacter(const wchar_t escChar)
 {
-	if(table.empty()) return str;
-	if(!str.compare(emptyStr)) return L"";
-	std::wstring result=str;
-	//restore substrings who equal rCodes. example: %1%1 -> %1
-	for(WSVector::iterator i=table.begin();i!=table.end();++i)
-	{
-		for (size_t index = 0; index = result.find(i->second, index), index != std::wstring::npos;)
-		{
-			if(result.substr(index+i->second.length(),i->second.length()).compare(i->second))
-			{
-				result.replace(index, i->second.length(), i->first);
-				index += i->first.length();
-			}
-			else
-			{
-				result.erase(index,i->second.length());
-				index +=i->second.length();
-			}
-		}
-	}
-	return result;
+	m_escChar = escChar;
 }
 
-std::wstring StringTranslator::encode(const std::wstring &str)
+wchar_t StringTranslator::getEscapeCharacter()
 {
-	if(table.empty()) return str;
-	if(str.empty()) return emptyStr;
-	std::wstring result = str;
-	if(!str.compare(emptyStr))
-	{
-		result+=emptyStr;
-		return result;
-	}
-	//duplicate substrings who equal rCodes. example: %1 -> %1%1
-	for(WSVector::iterator i=table.begin();i!=table.end();++i)
-	{
-		for (size_t index = 0; index = result.find(i->second, index), index != std::wstring::npos;)
-		{
-			std::wstring t = i->second;
-			t += i->second;
-			result.replace(index, i->second.length(), t);
-			index += t.length();
-		}
-	}
+	return m_escChar;
+}
 
-	//replace substrings who equal rCodes on eCodes. example: * -> %1
-	for(WSVector::iterator i=table.begin();i!=table.end();++i)
-	{
-		for (size_t index = 0; index = result.find(i->first, index), index != std::wstring::npos, index<=result.length();)
-		{
-			result.replace(index, i->first.length(), i->second);
-			index += i->second.length();
-		}
-	}
-	return result;
+void StringTranslator::setSequenceLenght(size_t seqLenght)
+{
+	m_seqLenght = seqLenght;
+}
+
+size_t StringTranslator::getSequenceLenght()
+{
+	return m_seqLenght;
+}
+
+void StringTranslator::setEmptyStringCode(const int code)
+{
+	m_emptyStr = code;
+}
+
+int StringTranslator::getEmptyStringCode()
+{
+	return m_emptyStr;
 }
 
 void StringTranslator::setTable(WSVector newTable)
@@ -81,15 +55,77 @@ void StringTranslator::dropTable()
 	table.clear();
 }
 
-void StringTranslator::addToTable(const std::wstring &first, const std::wstring &second)
+void StringTranslator::addToTable(const wchar_t first, const int second)
 {
 	table.push_back(WSPair(first,second));
 }
 
-void StringTranslator::setEmptySring(const std::wstring &es)
+std::wstring StringTranslator::decode(const std::wstring &str)
 {
-	emptyStr = es;
+	std::wstringstream stream;
+	size_t i=0;
+	while(i<str.length())
+	{
+		if(str[i]!=m_escChar)
+		{
+			stream << str[i];
+			++i;
+			continue;
+		}
+		if(str[i+1]==m_escChar)
+		{
+			stream << m_escChar;
+			i+=2;
+			continue;
+		}
+		int code = _wtoi(str.substr(i+1,m_seqLenght).data());
+		for(size_t j=0;j<table.size();++j)
+		{
+			if(table[j].second==code)
+			{
+				stream << table[j].first;
+				i+=m_seqLenght+1;
+				break;
+			}
+		}
+	}
+	std::wstring result;
+	result += stream.str();
+	return result;
 }
+
+std::wstring StringTranslator::encode(const std::wstring &str)
+{
+	std::wstringstream stream;
+	if(str.empty())
+	{
+		stream << m_escChar << std::setw(m_seqLenght) << std::setfill(L'0') << m_emptyStr;
+	}
+	else
+	{
+		if(table.empty()) return str;
+		for(int i=0;i<str.length();++i)
+		{
+			bool finded = false;
+			for(WSVector::iterator j=table.begin();j!=table.end();++j)
+			{
+				if(str[i]==j->first)
+				{
+					stream << m_escChar << std::setw(m_seqLenght) << std::setfill(L'0') << j->second;
+					finded = true;
+					break;
+				}
+			}
+			if(finded) continue;
+			if(str[i]==m_escChar) stream << m_escChar << m_escChar;
+			else stream << str[i];
+		}
+	}
+	std::wstring result;
+	result = stream.str();
+	return result;
+}
+
 
 std::wstring StringTranslator::fixPath(const std::wstring &str)
 {
--- StringTranslator.h
+++ StringTranslator.h
@@ -5,7 +5,7 @@
 class StringTranslator
 {
 public:
-	typedef std::pair<std::wstring,std::wstring> WSPair;
+	typedef std::pair<wchar_t,int> WSPair;
 	typedef std::vector<WSPair> WSVector;	
 	StringTranslator();
 	virtual ~StringTranslator();
@@ -13,13 +13,21 @@ public:
 	std::wstring decode(const std::wstring &str);
 	void setTable(WSVector newTable);
 	void dropTable();
-	void addToTable(const std::wstring &first, const std::wstring &second);
-	void setEmptySring(const std::wstring &es);
+	void addToTable(const wchar_t first, const int second);
 	std::wstring fixPath(const std::wstring &str);
 	std::wstring fixFilePath(const std::wstring &str);
+	void setEscapeCharacter(const wchar_t escChar);
+	wchar_t getEscapeCharacter();
+	void setSequenceLenght(size_t seqLenght);
+	size_t getSequenceLenght();
+
+	void setEmptyStringCode(const int code);
+	int getEmptyStringCode();
 protected:
 	WSVector table;
-	std::wstring emptyStr;
+	int m_emptyStr;
+	wchar_t m_escChar;
+	size_t m_seqLenght;
 };
 
 #endif __STRING_TRANSLATOR_H
